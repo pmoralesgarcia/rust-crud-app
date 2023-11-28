88c4dc4c31df1db6797bac524e100eb6236bc997
Pablo Morales <pablomorales@Pablo-Morales-Macbook-Pro.local>
2023-11-28 15:38:37 -0800

    update rust crud project with new code

--- src/main.rs
+++ src/main.rs
@@ -17,5 +17,26 @@ const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
 
+fn main() {
+    if let ERR(_) = set_database() {
+        println!("Error setting database");
+        return;
+    }
+
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    printlin!("Server listening on port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Unable to connect: {}", e);
+            }
+        }
+    }
+}
+
 #[macro_use]
 extern crate serde_drive;
